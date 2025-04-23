8c3c5d60a5ff80742b78bd56456b943678f21dac
networksaphyra <networksaphyra004@gmail.com>
2025-04-23 14:14:39 -0400

    feat: working prototype.

--- webserver.ino
+++ webserver.ino
@@ -7,7 +7,7 @@
 #include <WiFi.h>
 
 // Replace with your network credentials
-const char* ssid = "mihawk";
+const char* ssid = "ESP32_9548";
 const char* password = "mihawk2007";
 
 // Set web server port number to 80
@@ -21,44 +21,44 @@ String output26State = "off";
 String output27State = "off";
 
 // Assign output variables to GPIO pins
-// const int output26 = 26;
-// const int output27 = 27;
+//const int output26 = 26;
+//const int output27 = 27;
 
 // Current time
 unsigned long currentTime = millis();
 // Previous time
-unsigned long previousTime = 0; 
+unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
 void setup() {
   Serial.begin(115200);
   // Initialize the output variables as outputs
-  // pinMode(output26, OUTPUT);
-  // pinMode(output27, OUTPUT);
+  /*pinMode(output26, OUTPUT);
+  pinMode(output27, OUTPUT);*/
   // Set outputs to LOW
-  // digitalWrite(output26, LOW);
-  // digitalWrite(output27, LOW);
+  /*digitalWrite(output26, LOW);
+  digitalWrite(output27, LOW);*/
 
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  //WiFi.begin(ssid, password);// for Infrastructure mode
+  WiFi.softAP(ssid, password);// for Access Point mode
+  /*while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-  }
+  }*/
+  delay(5000);
   // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
-  // Serial.println(WiFi.localIP());
-  Serial.println(WiFi.softAPIP());
+  //Serial.println(WiFi.localIP());// Infra
+  Serial.println(WiFi.softAPIP());// AP
+
   server.begin();
 }
-
-// access point: acts as a router
-// infrastructure mode: connects to an access
 
 void loop(){
   WiFiClient client = server.available();   // Listen for incoming clients
@@ -84,59 +84,60 @@ void loop(){
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-            
+           
             // turns the GPIOs on and off
             if (header.indexOf("GET /26/on") >= 0) {
               Serial.println("GPIO 26 on");
               output26State = "on";
-              // digitalWrite(output26, HIGH);
+              //digitalWrite(output26, HIGH);
             } else if (header.indexOf("GET /26/off") >= 0) {
               Serial.println("GPIO 26 off");
               output26State = "off";
-              // digitalWrite(output26, LOW);
+              //digitalWrite(output26, LOW);
             } else if (header.indexOf("GET /27/on") >= 0) {
               Serial.println("GPIO 27 on");
               output27State = "on";
-              // digitalWrite(output27, HIGH);
+              //digitalWrite(output27, HIGH);
             } else if (header.indexOf("GET /27/off") >= 0) {
               Serial.println("GPIO 27 off");
               output27State = "off";
-              // digitalWrite(output27, LOW);
+              //digitalWrite(output27, LOW);
+
             }
-            
+           
             // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
+            // CSS to style the on/off buttons
             // Feel free to change the background-color and font-size attributes to fit your preferences
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
             client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
             client.println(".button2 {background-color: #555555;}</style></head>");
-            
+           
             // Web Page Heading
             client.println("<body><h1>ESP32 Web Server</h1>");
-            
+           
             // Display current state, and ON/OFF buttons for GPIO 26  
             client.println("<p>GPIO 26 - State " + output26State + "</p>");
-            // If the output26State is off, it displays the ON button       
+            // If the output26State is off, it displays the ON button      
             if (output26State=="off") {
               client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
+            }
                
             // Display current state, and ON/OFF buttons for GPIO 27  
             client.println("<p>GPIO 27 - State " + output27State + "</p>");
-            // If the output27State is off, it displays the ON button       
+            // If the output27State is off, it displays the ON button      
             if (output27State=="off") {
               client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
             } else {
               client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
             client.println("</body></html>");
-            
+           
             // The HTTP response ends with another blank line
             client.println();
             // Break out of the while loop
