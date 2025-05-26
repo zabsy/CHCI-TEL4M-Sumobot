82e293b3b7c364e826f39cd15f54f213a8a3f10d
Dmitry Vakulenko <dmitry.a.vakulenko@gmail.com>
2025-05-26 12:41:25 -0400

    WORKING PROTOTYPE

--- actual-code/main/main.ino
+++ actual-code/main/main.ino
@@ -1,21 +1,36 @@
+#include <ArduinoJson.h>
 #include <WiFi.h>
 #include <WebServer.h>
 #include <WebSocketsServer.h>
 
 #include "webServerFiles.h" // have to do this to fix a bug with the arduino ide preprocessor (i hate arduino ide)
 
+const int PWM1 = 5;
+const int PWM2 = 14;
+const int DIR1 = 4;
+const int DIR2 = 15;
+
 const char* ssid = "ESP32";
 const char* password = "Bigman2025";
 
+void controlMotors(int dir1State, int dir2State, int speed1, int speed2) {
+  digitalWrite(DIR1, dir1State);
+  digitalWrite(DIR2, dir2State);
+  analogWrite(PWM1, speed1);
+  analogWrite(PWM2, speed2);
+}
+
 WebServer server(80);
 WebSocketsServer ws = WebSocketsServer(81);
+
+float prevX, prevY;
 
 void handle_home_page() {
   server.send(200, "text/html", index_html);
 }
 
 void handle_main_js() {
-  server.send(200, "text/html", main_js);
+  server.send(200, "applications/javascript", main_js);
 }
 
 void handle_control(byte num, WStype_t type, uint8_t* payload, size_t length) {
@@ -27,10 +42,15 @@ void handle_control(byte num, WStype_t type, uint8_t* payload, size_t length) {
       Serial.println("Client " + String(num) + " disconnected.");
       break;
     case WStype_TEXT:
-      for (int i = 0; i < length; i++) {
-        Serial.print((char)payload[i]);
-      }
-      Serial.print("\n");
+      JsonDocument doc;
+      DeserializationError error = deserializeJson(doc, payload);
+      float x = doc["x"];
+      float y = doc["y"];
+
+      x=(x==0 ? 0: x<0 ? -1: 1);
+      y=(y==0 ? 0: y<0 ? -1: 1);
+      
+      controlMotors(x<0 ? LOW: HIGH, y<0 ? LOW: HIGH, x==0 ? 0:255, y==0 ? 0:255);
       break;
   }
 }
@@ -38,6 +58,14 @@ void handle_control(byte num, WStype_t type, uint8_t* payload, size_t length) {
 void setup() {
   Serial.begin(115200);
   while (!Serial);
+
+  pinMode(DIR1, OUTPUT);
+  pinMode(DIR2, OUTPUT);
+  pinMode(PWM1, OUTPUT);
+  pinMode(PWM2, OUTPUT);
+  prevX = 0;
+  prevY = 0;
+
   WiFi.softAP(ssid, password);
   IPAddress IP = WiFi.softAPIP();
 
@@ -57,3 +85,4 @@ void loop() {
   server.handleClient();
   ws.loop();
 }
+
